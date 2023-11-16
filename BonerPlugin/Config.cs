5dd1b1cb6271fd69a25481dc373217b78c4150e5
Zachuttak0 <150483761+Zachuttak0@users.noreply.github.com>
2023-11-16 16:29:09 -0500

    Initial commit

---
+++ BonerPlugin/Config.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace BonerPlugin
+{
+    public sealed class Config : IConfig
+    {
+        [Description("Turn the plugin on and off (true for on, false for off)")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Exiled makes me add 'Debug', no clue what it does so I wouldn't touch it")]
+        public bool Debug { get; set; } = false;
+        [Description("The default rule for this plugin ('false') is to not make 3114 a 100 percent spawn chance (leaving it to the default game system with a 30 percent chance) if someone spawns as 079, set this to 'true' to make the plugin ignore this rule (this should be used for events like 'the max players for the server is 100 for 2 hours')")]
+        public bool NoComputerOveride { get; set; } = false;
+        [Description("This sets what the playercount needs to be above to run the plugin (ex. if you want the pugin to run when there are 30 or more players on the server then set it to 29)")]
+        public int PlayersToRun { get; set; } = 29;
+    }
+}
---
+++ BonerPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BonerPlugin")]
+[assembly: AssemblyDescription("A plugin for SCP:SL")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BonerPlugin")]
+[assembly: AssemblyCopyright("Copyright @zachuttak0(discord) ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f0305e85-9ad4-49c3-aaac-19bfb69edc84")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
